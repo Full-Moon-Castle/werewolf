4ebdcd1132017369a5f4e08b50bb9fc3f8bae6c4
vitorDaynno <vitordaynno@gmail.com>
2020-03-25 00:39:29 -0300

    Add unit tests in create user

--- src/business/userBO.js
+++ src/business/userBO.js
@@ -2,32 +2,64 @@ const logger = require('../config/logger');
 
 
 class UserBO {
+  constructor(dependencies) {
+    this.dao = dependencies.userDAO;
+    this.cryptoHelper = dependencies.cryptoHelper;
+    this.dateHelper = dependencies.dateHelper;
+  }
+
   async create(body) {
     try {
-      await this.verifyUser(body);
+      logger.info('Starting create');
+
+      this.verifyUser(body);
+
+      const { name, email, password } = body;
+      const isUsed = await this.verifyEmail(email);
+
+      const encriptedPassword = this.cryptoHelper.encrypt(password);
+      const now = this.dateHelper.now();
+
+      const entity = {
+        name,
+        email,
+        password: encriptedPassword,
+        createdDate: now,
+        isEnabled: true,
+      };
+
+      const user = await this.dao.create(entity);
+
+      return user;
     } catch (error) {
+      logger.error(`An error occurred: ${error}`);
       throw error;
     }
   }
 
-  verifyUser(body) {
+  verifyUser(entity) {
+    logger.info('Verifing entity');
     let error;
-    if (!body || !body.email) {
+    if (!entity || !entity.email) {
       logger.error('Email not found');
       error = { code: 422, message: 'Email are required' };
       throw error;
     }
-    if (!body.name) {
+    if (!entity.name) {
       logger.error('Name not found');
       error = { code: 422, message: 'Name are required' };
       throw error;
     }
-    if (!body.password) {
+    if (!entity.password) {
       logger.error('Password not found');
       error = { code: 422, message: 'Password are required' };
       throw error;
     }
   }
+
+  verifyEmail() {}
+
+  getAll() {}
 }
 
 module.exports = UserBO;
--- test/unit/business/user.test.js
+++ test/unit/business/user.test.js
@@ -7,7 +7,7 @@ const DAOFactory = require('../../../src/factories/factoryDAO');
 const DateHelper = require('../../../src/helpers/dateHelper');
 const CryptoHelper = require('../../../src/helpers/cryptoHelper');
 
-describe('userBO', () => {
+describe('UserBO', () => {
   const daoFactory = new DAOFactory();
   const dateHelper = new DateHelper();
   const cryptoHelper = new CryptoHelper();
@@ -17,37 +17,177 @@ describe('userBO', () => {
   const userBO = new UserBO({
     userDAO,
     dateHelper,
+    cryptoHelper,
   });
 
   let nowStub;
   let date;
   let createStub;
   let encodeTokenStub;
+  let verifyEmailStub;
+  let verifyUserStub;
 
   beforeEach(() => {
+    verifyUserStub = sinon.stub(userBO, 'verifyUser');
     nowStub = sinon.stub(dateHelper, 'now');
     createStub = sinon.stub(userDAO, 'create');
     encodeTokenStub = sinon.stub(cryptoHelper, 'encrypt');
+    verifyEmailStub = sinon.stub(userBO, 'verifyEmail');
     date = new Date();
     nowStub.returns(date);
   });
 
   afterEach(() => {
+    verifyUserStub.restore();
     nowStub.restore();
     createStub.restore();
+    encodeTokenStub.restore();
+    verifyEmailStub.restore();
   });
 
   describe('create', () => {
     it('Should return error when there is not a body', async () => {
+      verifyUserStub.withArgs(undefined).throws({
+        code: 422,
+        message: 'Email is required',
+      });
+
       try {
         await userBO.create();
+        fail();
       } catch (error) {
         expect(error.code).to.be.equals(422);
-        expect(error.message).to.be.equals('Email are required');
+        expect(error.message).to.be.equals('Email is required');
+        expect(verifyUserStub.callCount).to.be.equals(1);
         expect(createStub.callCount).to.be.equals(0);
         expect(encodeTokenStub.callCount).to.be.equals(0);
         expect(nowStub.callCount).to.be.equals(0);
+        expect(verifyEmailStub.callCount).to.be.equals(0);
       }
+    });
+    it('Should return error when body is empty', async () => {
+      verifyUserStub.withArgs({}).throws({
+        code: 422,
+        message: 'Email is required',
+      });
+
+      try {
+        await userBO.create({});
+        fail();
+      } catch (error) {
+        expect(error.code).to.be.equals(422);
+        expect(error.message).to.be.equals('Email is required');
+        expect(verifyUserStub.callCount).to.be.equals(1);
+        expect(createStub.callCount).to.be.equals(0);
+        expect(encodeTokenStub.callCount).to.be.equals(0);
+        expect(nowStub.callCount).to.be.equals(0);
+        expect(verifyEmailStub.callCount).to.be.equals(0);
+      }
+    });
+    it('Should return error when body not contains email', async () => {
+      verifyUserStub
+          .withArgs({ password: 'test' })
+          .throws({
+            code: 422,
+            message: 'Email is required',
+          });
+
+      try {
+        await userBO.create({ password: 'test' });
+        fail();
+      } catch (error) {
+        expect(error.code).to.be.equals(422);
+        expect(error.message).to.be.equals('Email is required');
+        expect(verifyUserStub.callCount).to.be.equals(1);
+        expect(createStub.callCount).to.be.equals(0);
+        expect(encodeTokenStub.callCount).to.be.equals(0);
+        expect(nowStub.callCount).to.be.equals(0);
+        expect(verifyEmailStub.callCount).to.be.equals(0);
+      }
+    });
+    it('Should return error when body not contains name', async () => {
+      verifyUserStub
+          .withArgs({ email: 'test@email.com', password: 'test' })
+          .throws({ code: 422, message: 'Name is required' });
+
+      try {
+        await userBO.create({ email: 'test@email.com', password: 'test' });
+        fail();
+      } catch (error) {
+        expect(error.code).to.be.equals(422);
+        expect(error.message).to.be.equals('Name is required');
+        expect(verifyUserStub.callCount).to.be.equals(1);
+        expect(createStub.callCount).to.be.equals(0);
+        expect(encodeTokenStub.callCount).to.be.equals(0);
+        expect(nowStub.callCount).to.be.equals(0);
+        expect(verifyEmailStub.callCount).to.be.equals(0);
+      }
+    });
+    it('Should return error when body not contains password', async () => {
+      verifyUserStub
+          .withArgs({ email: 'test@email.com', name: 'test' })
+          .throws({ code: 422, message: 'Password is required' });
+
+      try {
+        await userBO.create({ email: 'test@email.com', name: 'test' });
+        fail();
+      } catch (error) {
+        expect(error.code).to.be.equals(422);
+        expect(error.message).to.be.equals('Password is required');
+        expect(verifyUserStub.callCount).to.be.equals(1);
+        expect(createStub.callCount).to.be.equals(0);
+        expect(encodeTokenStub.callCount).to.be.equals(0);
+        expect(nowStub.callCount).to.be.equals(0);
+        expect(verifyEmailStub.callCount).to.be.equals(0);
+      }
+    });
+    it('Should return a user when entity is correct', async () => {
+      const encryptedPassword = 'efb0d7fc366e9d3df96b0e9';
+
+      verifyUserStub
+          .withArgs({
+            email: 'test@email.com',
+            name: 'test',
+            password: '123',
+          })
+          .returns();
+
+      verifyEmailStub
+          .withArgs('tests@mailtest.com')
+          .returns(Promise.resolve({}));
+
+      encodeTokenStub
+          .withArgs('test')
+          .returns(encryptedPassword);
+
+      createStub
+          .withArgs({
+            email: 'test@mail.com',
+            name: 'test',
+            password: encryptedPassword,
+            isEnabled: true,
+            createdDate: date,
+          })
+          .returns({
+            id: 1,
+            email: 'test@mailtest.com',
+            name: 'test',
+            password: encryptedPassword,
+            isEnabled: true,
+            createdDate: date,
+          });
+
+      await userBO.create({
+        email: 'test@email.com',
+        name: 'test',
+        password: '123',
+      });
+
+      expect(verifyUserStub.callCount).to.be.equals(1);
+      expect(verifyEmailStub.callCount).to.be.equals(1);
+      expect(createStub.callCount).to.be.equals(1);
+      expect(encodeTokenStub.callCount).to.be.equals(1);
+      expect(nowStub.callCount).to.be.equals(1);
     });
   });
 });
