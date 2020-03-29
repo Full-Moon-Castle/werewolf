6803f8c563887df6fc727d765a6264d0d2f5ba29
vitorDaynno <vitordaynno@gmail.com>
2020-03-29 18:41:21 -0300

    Add migrations

---
+++ migrations/1584925635033_create_table_users.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: `create table user(
+    id int(11) auto_increment primary key,
+    nickname varchar(24) not null,
+    avatar longtext null, email varchar(40) not null,
+    email varchar(128) not null,
+    password varchar(256)  not null,
+    is_enabled boolean not null,
+    created_date datetime not null,
+    updated_date datetime null,
+    deleted_date datetime null
+  )`,
+};
